d137406f031d741ff5b7bf281c189695995af651
dinod tharinda <dinodtharinda2001@gmail.com>
2024-05-21 22:54:56 +0530

    Initial Commit

---
+++ app/src/main/java/edu/hardwork/workoutapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package edu.hardwork.workoutapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import edu.hardwork.workoutapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
